12d29d6eb73f2755994aa6572c4e80693d7a54aa
Bullets-jyr <jyr@bullets.co.kr>
2023-09-25 10:01:06 +0900

    Go Named 함수

--- lib/screen/root_screen.dart
+++ lib/screen/root_screen.dart
@@ -16,6 +16,20 @@ class RootScreen extends StatelessWidget {
             },
             child: Text('Go Basic'),
           ),
+          ElevatedButton(
+            onPressed: () {
+              context.goNamed('named_screen');
+              // /123/456/789/10/11/12/13/14/15/16/17/18/19/20
+              // /long_screen
+            },
+            child: Text('Go Named'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              context.go('/push');
+            },
+            child: Text('Go Push'),
+          ),
         ],
       ),
     );
